626c3691e91d6e6e781b9d8296168449e523d45e
R1chardD0e <doerichard190@gmail.com>
2025-11-03 00:56:50 +0100

    Implement tab functionality in App component and define Tabs component

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,24 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
+  return (
+    <ul>
+      {tabs.map(tab => (
+        <li
+          key={tab.id}
+          className={activeTabId === tab.id ? 'is-active' : ' '}
+          data-cy="Tab"
+        >
+          <a
+            href={`#${tab.id}`}
+            data-cy="TabLink"
+            onClick={e => {
+              e.preventDefault();
+              if (tab.id !== activeTabId) onTabSelected(tab.id);
+            }}
+          >
+            {tab.title}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
